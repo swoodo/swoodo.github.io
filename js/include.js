6a986c522ab7683dbaac250f53de23783a074e07
swoodo <tictaks@gmail.com>
2019-11-20 11:43:21 -0800

    new stuff

--- js/include.js
+++ js/include.js
@@ -1,7 +1,7 @@
 var main = function() {
-
+  $("#contact-form").load("contact-form.html"); 
   $("#footer").load("footer.html"); 
-
+  $("#nav").load("navigation.html"); 
 }
 
 $(document).ready(main);
